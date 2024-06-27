d40b33c1d32d4d4b7d3c3964332d4df3762b3d43
Luke Wait <123434265+LukeWait@users.noreply.github.com>
2024-06-28 07:55:08 +1000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -93,17 +93,17 @@ installDepend() {
             sudo nala update -yq
 
             # Install python/pip
-            sudo nala install python3-dev python3-pip python3-setuptools
+            sudo nala install -yq python3-dev python3-pip python3-setuptools
 
             # Update tldr pages
             tldr -u
 
             # Fetch the latest fastfetch release URL for linux-amd64 deb file
-			FASTFETCH_URL=$(curl -s https://api.github.com/repos/fastfetch-cli/fastfetch/releases/latest | grep "browser_download_url.*linux-amd64.deb" | cut -d '"' -f 4)
-			# Download the latest fastfetch deb file
-			curl -sL $FASTFETCH_URL -o /tmp/fastfetch_latest_amd64.deb
-			# Install the downloaded deb file using nala
-			sudo nala install -yq /tmp/fastfetch_latest_amd64.deb
+            FASTFETCH_URL=$(curl -s https://api.github.com/repos/fastfetch-cli/fastfetch/releases/latest | grep "browser_download_url.*linux-amd64.deb" | cut -d '"' -f 4)
+            # Download the latest fastfetch deb file
+            curl -sL $FASTFETCH_URL -o /tmp/fastfetch_latest_amd64.deb
+            # Install the downloaded deb file using nala
+            sudo nala install -yq /tmp/fastfetch_latest_amd64.deb
             ;;
         zypper)
             sudo zypper install -y ${DEPENDENCIES}
